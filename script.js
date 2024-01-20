cb32a777fd14537ad2ce7e5d3980bf1cc61c101a
Ali Hamza <alihamza022bb@gmail.com>
2024-01-20 15:25:24 +0500

    committed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const user = {
     username: "Ali",
     loginCount: 5,
     getUserDetails: function() {
-        console.log("Got the details")
+        console.log("Value: ", `${this.loginCount}`)
     }
 }
 
